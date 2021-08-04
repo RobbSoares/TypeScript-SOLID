bab8e29862909e6a25166842029fb744b01dafb9
Robson Soares <robson.santana.soares@gmail.com>
2021-08-04 15:52:12 -0300

    ISP - Interface Segregation Principle

---
+++ src/isp/main.ts
@@ -0,0 +1,28 @@
+/*
+  Interface segregation principle (Princípio da segregação de interface) - os clientes não devem ser forçados a depender de types, interfaces ou membros abstratos que não utilizam
+*/
+
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const cart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(cart, messaging, persistency);
+
+cart.addItem(new Product('Camiseta', 20.99));
+cart.addItem(new Product('Lápis', 1.99));
+cart.addItem(new Product('Relógio', 49.99));
+
+console.log(cart.items);
+console.log(cart.total());
+console.log(cart.totalWithDiscount());
+order.checkout();
+console.log(order.orderStatus);
